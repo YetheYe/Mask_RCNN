f85d8ee287c0bafce05a15ff9ab00413620978ba
Brandon Lees <brandon@deepmagic.cc>
2018-04-01 10:34:53 +0000

    make return objects into class to perform initialization once and allow multiple calls to label image

--- return_objects.py
+++ return_objects.py
@@ -21,25 +21,29 @@ class BagsConfig(Config):
     IMAGES_PER_GPU = 1
     NUM_CLASSES = 1 + 12  # background [index: 0] + 1 person class tranfer from COCO [index: 1] + 12 classes
 
-def return_objects(img_path, json_file, model_path):
+class Hans1:
 
-    config = BagsConfig()
-    
-    image = cv2.imread(img_path)
-    model = modellib.MaskRCNN(mode="inference", model_dir=os.path.dirname(model_path), config=config)
+    def __init__(self, json_file, model_path):
+        config = BagsConfig()
 
-    model.load_weights(model_path, by_name=True)
-    
-    class_names = ['BG'] + json.load(open(json_file))['classes']
-    
-    # ## Run Object Detection
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    results = model.detect([image], verbose=0)
+        self.model = modellib.MaskRCNN(mode="inference", model_dir=os.path.dirname(model_path), config=config)
+        self.model.load_weights(model_path, by_name=True)
 
-    # Visualize results
-    r = results[0]
+        self.class_names = [ 'BG' ] + json.load(open(json_file))['classes']
+
+
+    def return_objects(self, img_path):
+        image = cv2.imread(img_path)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+        # ## Run Object Detection
+        results = self.model.detect([image], verbose=0)
+
+        # Visualize results
+        r = results[0]
         
-    return [class_names[i] for i in r['class_ids']]
+        return [self.class_names[i] for i in r['class_ids']]
 
 if __name__ == '__main__':
-    print (return_objects('1top1.png', 'model/pascal_dataset.json', 'model/mask_rcnn_bags_0005.h5'))
+    ai = Hans1('model/pascal_dataset.json', 'model/mask_rcnn_bags_0005.h5')
+    print(ai.return_objects('1top1.png'))
